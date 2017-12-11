b372d9c9923a0b5f620f0adc8f1125025177431e
cdesimone18 <cdesimone18@stjohnsprep.org>
2017-12-11 15:55:06 -0500

    Editable Table

--- tournamentDialogs.py
+++ tournamentDialogs.py
@@ -12,9 +12,22 @@ class ManualMatchesDialog(QDialog):
         
         layout = QVBoxLayout()
         layout.setContentsMargins(0,0,0,0)
-        self.tableView = QTableView()
-        self.initTable(self.tableView)
-        layout.addWidget(self.tableView)
+        #self.tableView = QTableView()
+        # self.initTable(self.tableView)
+        self.tableWidget = VexMatchesTable()
+        self.initTableWidget(self.tableWidget)
+        layout.addWidget(self.tableWidget)
+        
+        buttonsLayout = QHBoxLayout()
+        self.buttonOK = QPushButton()
+        self.buttonOK.setText("OK")
+        self.buttonCancel = QPushButton()
+        self.buttonCancel.setText("Cancel")
+        buttonsLayout.addWidget(self.buttonOK)
+        buttonsLayout.addWidget(self.buttonCancel)
+        buttonsLayout.setContentsMargins(0, 0, 0, 0)
+        layout.addLayout(buttonsLayout)
+        
         self.setLayout(layout)
         # self.addWidget(self.tableView)
     
@@ -24,65 +37,26 @@ class ManualMatchesDialog(QDialog):
     def getMatches(self):
         pass
         
-    def initTable(self, tableView):
-        self.tableData = [['Q1', '9228A', '9228B', '9228C', '9228D']]
-        header = ['Match #', 'Red 1', 'Red 2', 'Blue 1', 'Blue 2']
-        tableModel = VexMatchModel(self.tableData, header, self) 
-        tableView.setModel(tableModel)
-        tableView.setMinimumSize(500, 250)
-        
-        # hide vertical header
-        vh = tableView.verticalHeader()
-        vh.setVisible(False)
+    def initTableWidget(self, tableWidget):
+    	tableWidget.setMinimumSize(505, 600)
 
-        # set horizontal header properties
-        hh = tableView.horizontalHeader()
-        hh.setStretchLastSection(True)
-        
-        # set column width to fit contents
-        tableView.resizeColumnsToContents()
-        
-        # set row height
-        nrows = len(self.tableData)
-        for row in range(nrows):
-            tableView.setRowHeight(row, 18)
-            
-        # enable sorting
-        tableView.setSortingEnabled(True)
-        tableView.setEditTriggers(QAbstractItemView.DoubleClicked)
-        
-class VexMatchModel(QAbstractTableModel): 
-    def __init__(self, datain, headerdata, parent=None, *args): 
-        """ datain: a list of lists
-            headerdata: a list of strings
-        """
-        QAbstractTableModel.__init__(self, parent, *args) 
-        self.arraydata = datain
-        self.headerdata = headerdata
+class VexMatchesTable(QTableWidget):
+    def __init__(self, *args, data=None):
+        QTableWidget.__init__(self, *args)
+        self.data = data
+        self.verticalHeader().setVisible(False)
+        self.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
+        self.verticalHeader().setDefaultSectionSize(25);
+        self.horizontalHeader().setStretchLastSection(True)
+        self.setColumnCount(5)
+        self.setRowCount(250)
+        headerLabels = ['Match #', 'Red 1', 'Red 2', 'Blue 1', 'Blue 2']
+        self.setHorizontalHeaderLabels(headerLabels)
+        self.setSortingEnabled(True)
+        # self.setData()
+        # self.resizeColumnsToContents()
+        # self.resizeRowsToContents()
  
-    def rowCount(self, parent): 
-        return len(self.arraydata) 
- 
-    def columnCount(self, parent): 
-        return len(self.arraydata[0]) 
- 
-    def data(self, index, role): 
-        if not index.isValid(): 
-            return QVariant() 
-        elif role != Qt.DisplayRole: 
-            return QVariant() 
-        return QVariant(self.arraydata[index.row()][index.column()]) 
-
-    def headerData(self, col, orientation, role):
-        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return QVariant(self.headerdata[col])
-        return QVariant()
-
-    def sort(self, Ncol, order):
-        """Sort table by given column number.
-        """
-        # self.emit(SIGNAL("layoutAboutToBeChanged()"))
-        self.arraydata = sorted(self.arraydata, key=operator.itemgetter(Ncol))        
-        if order == Qt.DescendingOrder:
-            self.arraydata.reverse()
-        # self.emit(SIGNAL("layoutChanged()"))
+    # initializes the table with a list of (team name red 1, team name red 2, ...)
+    def loadData(self, teams):
+        pass
